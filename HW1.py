baca8898db557f843a7eefcb2daff7d0aa4ea822
Fabian Tan <fnever520@git.com>
2019-03-18 18:28:46 +0900

    Amend

--- HW1.py
+++ HW1.py
@@ -1,23 +1,28 @@
-import random
-
-def init():
-	x = 0 
+def Setup():
+	x, minx, maxn = 0, -1, -1 
 	numOfZombies = int(input('Enter the number of Zombies: ' ))
 	lengthOfBridge = int(input('Enter length of the bridge: '))
 	posOfZombies = [None] * int(numOfZombies)
-	positionOfZombies = input('Initial position of the zombie:')
+	positionOfZombies = input('Place the initial position of each individual zombie:')
 	for i in positionOfZombies.split(' '):
 		posOfZombies[x] = i
 		x+=1
+	#TODO: catch exception
+	for p in posOfZombies:
+		if p == None:
+			print("Something is amiss! ")
+			break
+		elif int(p) <= 0 or int(p) > lengthOfBridge:
+			print("Wrong position was inputted...The val should be in the range of (1, lengthOfBridge)")
+			return 1 
 
-	minx = -1 
-	maxn = -1
 	for z in range(numOfZombies):
-		print('max:' ,maxn)
-		print('min:' , minx)
-		maxn = max(maxn, max(lengthOfBridge-int(posOfZombies[z])+1, int(posOfZombies[z])+1))
+		# print('max:' ,maxn)
+		# print('min:' , minx)
+		maxn = max(maxn, max(lengthOfBridge-int(posOfZombies[z])+1, int(posOfZombies[z])))
 		minx = max(minx, min(lengthOfBridge-int(posOfZombies[z]), int(posOfZombies[z])))
-	print('Total time taken : ', maxn, minx)
+	print('The earliest time taken is : ', minx)
+	print('The latest time taken is : ', maxn)
 
 # def Setup():
 # 	pickDirection = random.randint(1, 100)
@@ -34,7 +39,12 @@ def init():
 # 	self.numOfZombies =
 # 	self.lengthOfBridge = 
 # 	self.positionOfZombies = 
+# 	both of the zombies meet at the same step from different steps, it will take one sec and another sec to bounce off
+#	eg.  #####################   1s   ######################   1s   #####################
+#		 #   # 1 #   # 2 #   #  --->  #   #   # 12 #   #   #  --->  #   # 1 #   # 2 #   #
+#		 #####################	      ######################	    #####################
+# 	if both of the zombies are adjacent to each other, it will only take one sec and change direction instantly.
+
 
 if __name__ == '__main__':
-	init()
-	# Setup()
+	Setup()
